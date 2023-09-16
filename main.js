40ec05e7bd4e1dad6b770c21780b6172678887ef
achampoux <22298638+achampoux@users.noreply.github.com>
2023-09-16 14:51:05 -0400

    Added function operate and main variables

--- main.js
+++ main.js
@@ -5,3 +5,27 @@ function subtract(a,b) { return a-b }
 function multiply(a,b) { return a*b }
 
 function divide(a,b) { return a/b }
+
+var number1;
+var number2;
+var operator;
+
+function operate(a,b,operator){
+    switch (operator) {
+        case '+':
+            return add(a,b);
+            break;
+        case '-':
+            return subtract(a,b);
+            break;
+        case '*':
+            return multiply(a,b);
+            break;
+        case '/':
+            return divide(a,b);
+            break;
+    
+        default:
+            break;
+    }
+}
