deae261e8f08975d79240896b0664a421feaf5ee
achampoux <22298638+achampoux@users.noreply.github.com>
2023-09-17 13:25:53 -0400

    Added key listeners and display

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ function divide(a,b) { return a/b }
 var number1;
 var number2;
 var operator;
+var bottomDisplay = '';
 
 function operate(a,b,operator){
     switch (operator) {
@@ -29,3 +30,33 @@ function operate(a,b,operator){
             break;
     }
 }
+
+function keyListener(id, display) {
+    let element = document.getElementById(id)
+    element.addEventListener('click', (e) => {
+        displayBottomRow(display)
+    });
+}
+
+function displayBottomRow(a){
+    let element = document.getElementById("bottom-row");
+    bottomDisplay += a;
+    element.innerHTML = bottomDisplay;
+}
+
+keyListener(7, 7);
+keyListener(8, 8);
+keyListener(9, 9);
+keyListener('divide', '÷');
+keyListener(4, 4);
+keyListener(5, 5);
+keyListener(6, 6);
+keyListener('multiply', 'x');
+keyListener(1, 1);
+keyListener(2, 2);
+keyListener(3, 3);
+keyListener('subtract', '-');
+keyListener('dot', '.');
+keyListener(0, 0);
+keyListener('equal', '=');
+keyListener('add', '+');
